d735422c5054a1185c907e9283fa05b7933410ca
Lamia <dumbdashop@gmail.com>
2025-06-27 17:07:52 +0200

    Fly Mod derived from Slide Mod
    Git Init + naming flymod etc

---
+++ Fly Mod/Fly Mod/FlyMod.cs
@@ -0,0 +1,66 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Photon.Pun;
+using UnityEngine;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+[BepInPlugin("com.lamia.flymod", "FlyMod", "1.0.0")]
+public class FlyMod : BaseUnityPlugin
+{
+    internal static ManualLogSource Log;
+
+    private void Awake()
+    {
+        Log = Logger;
+        Log.LogInfo("FlyMod loaded.");
+        Harmony harmony = new Harmony("com.lamia.flymod");
+        harmony.PatchAll();
+    }
+}
+
+[HarmonyPatch(typeof(Character), "Awake")]
+public static class PalThrow
+{
+    [HarmonyPostfix]
+    public static void AwakePatch(Character __instance)
+    {
+        if (!__instance.IsLocal)
+            return;
+        if ((Object)(object)((Component)__instance).GetComponent<FlyModPatch>() == (Object)null)
+        {
+            ((Component)__instance).gameObject.AddComponent<FlyModPatch>();
+            FlyMod.Log.LogInfo((object)("FlyModPatch added to: " + ((Object)__instance).name));
+        }
+    }
+}
+
+public class FlyModPatch : MonoBehaviourPun
+{
+    private Character character;
+    private CharacterMovement charMovement;
+    private void Start()
+    {
+        character = ((Component)this).GetComponent<Character>();
+        charMovement = this.GetComponent<CharacterMovement>();
+    }
+
+    private void Update()
+    {
+        if (!character.IsLocal)
+
+            return;
+        if (Input.GetKey(KeyCode.LeftAlt))
+        {
+            Vector3 flyForce = character.data.lookDirection * 100f;
+            Vector3 momentumForce = character.data.avarageLastFrameVelocity + flyForce;
+            foreach (var part in character.refs.ragdoll.partList)
+            {
+                part.AddForce(flyForce, ForceMode.Force);
+            }
+            FlyMod.Log.LogInfo("Slided with " + flyForce);
+
+        }
+
+    }
+}
