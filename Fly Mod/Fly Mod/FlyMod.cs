33aa4a726b169309b90560290d2d470ecc352f32
Lamia <dumbdashop@gmail.com>
2025-06-27 19:07:59 +0200

    Last changes before Version 1.0.0
    No I only have to add a config file to release v1.0.0

--- Fly Mod/Fly Mod/FlyMod.cs
+++ Fly Mod/Fly Mod/FlyMod.cs
@@ -39,13 +39,13 @@ public class FlyModPatch : MonoBehaviourPun
 {
     private Character character;
     private CharacterMovement charMovement;
-    private float maxGravity;
-    private float flyMaxGravity = -1f;
+    // private float maxGravity;
+    // private float flyMaxGravity = -1f;
     private void Start()
     {
         character = ((Component)this).GetComponent<Character>();
         charMovement = this.GetComponent<CharacterMovement>();
-        maxGravity = charMovement.maxGravity;
+        // maxGravity = charMovement.maxGravity;
     }
 
     private void Update()
@@ -53,29 +53,65 @@ public class FlyModPatch : MonoBehaviourPun
         if (!character.IsLocal)
 
             return;
-        if (Input.GetKey(KeyCode.Mouse4))
+        if (Input.GetKey(KeyCode.Mouse3))
         {
-            charMovement.maxGravity = flyMaxGravity;
-            Vector3 flyForce = character.data.lookDirection * 100f;
+            // charMovement.maxGravity = flyMaxGravity;
+            character.data.isGrounded = true;
+
+
+            Vector3 flyForce = character.data.lookDirection;
 
             if (Input.GetKey(KeyCode.W))
-                flyForce *= 2f;
-            if (Input.GetKey(KeyCode.S))
-                flyForce *= -1f;
+                flyForce *= 800f;
 
-            flyForce.x = Mathf.Clamp(flyForce.x, -600f, 600f);
-            flyForce.y = Mathf.Clamp(flyForce.y, -600f, 600f);
-            flyForce.z = Mathf.Clamp(flyForce.z, -600f, 600f);
+            else if (Input.GetKey(KeyCode.S))
+                flyForce *= -800f;
+
+
+            Vector3 right = Vector3.Cross(Vector3.up, character.data.lookDirection).normalized * 800f;
+
+            if (Input.GetKey(KeyCode.D))
+                flyForce += right; 
+
+            if (Input.GetKey(KeyCode.A))
+                flyForce -= right; 
+
+
+            if (Input.GetKey(KeyCode.Space))
+                flyForce += Vector3.up * 800f;
+
+
+            else if (Input.GetKey(KeyCode.LeftControl))
+                flyForce += Vector3.down * 800f;
+
+
+
+
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                character.AddStamina(charMovement.sprintStaminaUsage * Time.deltaTime);
+                flyForce *= 4f;
+            }
+
+
+            flyForce += Vector3.down * 100f;
+
+            flyForce.x = Mathf.Clamp(flyForce.x, -3600f, 3600f);
+            flyForce.y = Mathf.Clamp(flyForce.y, -3600f, 3600f);
+            flyForce.z = Mathf.Clamp(flyForce.z, -3600f, 3600f);
             foreach (var part in character.refs.ragdoll.partList)
             {
-                part.AddForce(flyForce, ForceMode.Force);
+                //if (dirInput)
+                    part.AddForce(flyForce, ForceMode.Force);
+
             }
             FlyMod.Log.LogInfo("Flew with " + flyForce);
 
         }
         else
         {
-            charMovement.maxGravity = maxGravity;
+            // charMovement.maxGravity = maxGravity;
         }
 
     }
