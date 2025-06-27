1373a7ad2b52f51874d4fe8a309857942087d2bf
Lamia <dumbdashop@gmail.com>
2025-06-27 22:21:33 +0200

    New Flight Mode added
    Its more like a jetpack instead of the creative flying

--- Fly Mod/Fly Mod/FlyMod.cs
+++ Fly Mod/Fly Mod/FlyMod.cs
@@ -6,7 +6,7 @@ using Photon.Pun;
 using UnityEngine;
 using static System.Runtime.CompilerServices.RuntimeHelpers;
 
-[BepInPlugin("com.lamia.flymod", "FlyMod", "1.0.0")]
+[BepInPlugin("com.lamia.flymod", "FlyMod", "1.1.0")]
 public class FlyMod : BaseUnityPlugin
 {
     internal static ConfigEntry<float> BaseForce;
@@ -16,6 +16,7 @@ public class FlyMod : BaseUnityPlugin
     internal static ConfigEntry<float> MaxClamp;
     internal static ConfigEntry<KeyCode> FlyKey;
     internal static ConfigEntry<bool> ToggleFly;
+    internal static ConfigEntry<bool> CreativeFlyMode;
     internal static ManualLogSource Log;
 
     private void Awake()
@@ -24,12 +25,13 @@ public class FlyMod : BaseUnityPlugin
         Log.LogInfo("FlyMod loaded.");
         Harmony harmony = new Harmony("com.lamia.flymod");
         harmony.PatchAll();
+        CreativeFlyMode = Config.Bind("FlyMod", "CreativeFlyMode", true, "If true, flying feels more like noclipping in Garry's Mod or manuevering a 3d environment. Setting this to false makes flying feel more like being boosted by a directional Jetpack. You should disable ToggleFly if this is set to false.");
         FlyKey = Config.Bind("FlyMod", "FlyKey", KeyCode.V, "Key to activate flying (default: Mouse3/Middle mouse button).");
         ToggleFly = Config.Bind("FlyMod", "ToggleFly", true, "If true, fly is toggled on/off with the key instead of holding.");
         BaseForce = Config.Bind("FlyMod", "BaseForce", 800f, "Base force applied when flying forward/backward/sideways.");
         VerticalForce = Config.Bind("FlyMod", "VerticalForce", 800f, "Force applied when flying up or down.");
         SprintMultiplier = Config.Bind("FlyMod", "SprintMultiplier", 4f, "Multiplier when holding shift.");
-        MaxClamp = Config.Bind("FlyMod", "MaxClamp", 3600f, "Clamp for maximum fly force in any direction.");
+        MaxClamp = Config.Bind("FlyMod", "MaxClamp", 3600f, "Clamp for maximum fly speed in any direction.");
 
     }
 }
@@ -55,15 +57,15 @@ public class FlyModPatch : MonoBehaviourPun
 {
     private Character character;
     private CharacterMovement charMovement;
-    // private float maxGravity;
-    // private float flyMaxGravity = -1f;
+    //private float maxGravity;
+    //private float flyMaxGravity = -1f;
     private bool flyActive = false;
 
     private void Start()
     {
         character = ((Component)this).GetComponent<Character>();
-        charMovement = this.GetComponent<CharacterMovement>();
-        // maxGravity = charMovement.maxGravity;
+        //charMovement = this.GetComponent<CharacterMovement>();
+        //maxGravity = charMovement.maxGravity;
     }
 
     private void Update()
@@ -81,62 +83,108 @@ public class FlyModPatch : MonoBehaviourPun
         }
         if (Input.GetKey(FlyMod.FlyKey.Value) || flyActive)
         {
-            // charMovement.maxGravity = flyMaxGravity;
-            character.data.isGrounded = true;
+
+                
+
+            if (FlyMod.CreativeFlyMode.Value)
+                character.data.isGrounded = true;
+            else
+            {
+                character.data.sinceGrounded = 0.5f;
+                //charMovement.maxGravity = flyMaxGravity;
+            }
 
 
-            Vector3 flyForce = character.data.lookDirection;
+                Vector3 flyForce = character.data.lookDirection;
 
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetKey(KeyCode.W) || !FlyMod.CreativeFlyMode.Value)
+            {
                 flyForce *= FlyMod.VerticalForce.Value;
+            }
+                
 
-            else if (Input.GetKey(KeyCode.S))
+            else if (Input.GetKey(KeyCode.S) && FlyMod.CreativeFlyMode.Value)
+            {
                 flyForce *= -FlyMod.VerticalForce.Value;
+            }
+                
 
 
             Vector3 right = Vector3.Cross(Vector3.up, character.data.lookDirection).normalized * FlyMod.VerticalForce.Value;
 
             if (Input.GetKey(KeyCode.D))
-                flyForce += right; 
+            {
+                if (FlyMod.CreativeFlyMode.Value)
+                    flyForce += right;
+                else
+                    flyForce += right / 8;
+            }
+                
 
             if (Input.GetKey(KeyCode.A))
-                flyForce -= right; 
+            {
+                if (FlyMod.CreativeFlyMode.Value)
+                    flyForce -= right;
+                else
+                    flyForce -= right / 8;
+            }
+                
 
 
             if (Input.GetKey(KeyCode.Space))
-                flyForce += Vector3.up * FlyMod.VerticalForce.Value;
+            {
+                if(FlyMod.CreativeFlyMode.Value)
+                    flyForce += Vector3.up * FlyMod.VerticalForce.Value;
+                else
+                    flyForce += Vector3.up * (FlyMod.VerticalForce.Value/8);
+            }
+                
 
 
-            else if (Input.GetKey(KeyCode.LeftControl))
+            else if (Input.GetKey(KeyCode.LeftControl) && FlyMod.CreativeFlyMode.Value)
+            {
                 flyForce += Vector3.down * FlyMod.VerticalForce.Value;
+            }
+                
 
 
 
 
 
-            if (Input.GetKey(KeyCode.LeftShift))
+            if (Input.GetKey(KeyCode.LeftShift) && FlyMod.CreativeFlyMode.Value)
             {
                 character.AddStamina(charMovement.sprintStaminaUsage * Time.deltaTime);
                 flyForce *= FlyMod.SprintMultiplier.Value;
             }
 
+            if (FlyMod.CreativeFlyMode.Value)
+                flyForce += Vector3.down * 100f;
+            else
+                flyForce += Vector3.up * 100f;
 
-            flyForce += Vector3.down * 100f;
 
             flyForce.x = Mathf.Clamp(flyForce.x, -FlyMod.MaxClamp.Value, FlyMod.MaxClamp.Value);
             flyForce.y = Mathf.Clamp(flyForce.y, -FlyMod.MaxClamp.Value, FlyMod.MaxClamp.Value);
             flyForce.z = Mathf.Clamp(flyForce.z, -FlyMod.MaxClamp.Value, FlyMod.MaxClamp.Value);
             foreach (var part in character.refs.ragdoll.partList)
             {
-                //if (dirInput)
+                if (FlyMod.CreativeFlyMode.Value)
                     part.AddForce(flyForce, ForceMode.Force);
+                else
+                {
+                    if (!part.partType.ToString().Contains("Leg") && !part.partType.ToString().Contains("Knee"))
+                    {
+                        part.AddForce(flyForce / 8, ForceMode.Acceleration);
+                    }
+                }
+                    
 
             }
 
         }
         else
         {
-            // charMovement.maxGravity = maxGravity;
+
         }
 
     }
